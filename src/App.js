2b6b0790f6dd81b9ee98715fbede062453d0d80a
Mouataz Saadidi <saadidimouataz@gmail.com>
2023-02-11 12:58:53 +0100

    test1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Nav from "./components/Nav";
+import Promo from './components/Promo';
+import Intro1 from './components/Intro1';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Footer from './components/Footer';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+  return ( 
+    <div className="App"> 
+      <Nav/>
+      <Promo/>
+      <Intro1/>
+      <Intro2/>
+      <Intro3/>
+      <Footer/>
+    </div> 
+  ); 
+} 
+ 
 export default App;
---
+++ src/components/Promo.js
@@ -0,0 +1,14 @@
+function Promo() {
+    return (
+        <div className="promo-section">
+            <div>
+                <h1>Don't miss this deal!</h1>
+                </div>
+                <div>
+                <h2>Subscribe to my newsletter and get all the shop items at 50% off!</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Promo;
