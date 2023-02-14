7547047dc7aa4f109b200c3be002e84dcdf15050
Mouataz Saadidi <saadidimouataz@gmail.com>
2023-02-15 00:47:12 +0100

    react player added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import Intro1 from './components/Intro1';
 import Intro2 from './components/Intro2';
 import Intro3 from './components/Intro3';
 import Footer from './components/Footer';
+import ReactPlayer from 'react-player';
+
 function App() {
+  const vidUrl = "https://www.youtube.com/watch?v=ihZDdExto_I";
   return ( 
-    <div className="App"> 
+    <div className="App" > 
       <Nav/>
       <Promo/>
+      <ReactPlayer
+        url={vidUrl}
+        playing={false}
+        controls
+        volume={0.5} />
       <Intro1/>
       <Intro2/>
       <Intro3/>
